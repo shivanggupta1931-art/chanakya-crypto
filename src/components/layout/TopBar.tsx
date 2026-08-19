a3624306c98e7ac50e73009be29b5b6a6f4a33d0
shivanggupta1931-art <shivanggupta1931@gmail.com>
2026-08-19 18:36:45 +0530

    Remove demo mock data label

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -9,7 +9,6 @@ export default function TopBar() {
         <kbd>⌘ K</kbd>
       </div>
       <div className="topbar-right">
-        <span className="demo-pill">DEMO / MOCK DATA</span>
         <button className="icon-btn"><Bell size={18}/><i>4</i></button>
         <div className="secure-status"><ShieldCheck size={16}/> SECURE SESSION</div>
       </div>
